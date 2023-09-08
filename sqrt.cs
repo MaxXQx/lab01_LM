4793f8f12042148f0343c8d9a03aa633c8580330
MaxXQx <144465756+MaxXQx@users.noreply.github.com>
2023-09-08 17:53:18 +0300

    Add files via upload

---
+++ sqrt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lap01_LM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d = 4.0;
+            double f = Math.Sqrt(d);
+            Console.WriteLine(f);
+        }
+    }
+}
