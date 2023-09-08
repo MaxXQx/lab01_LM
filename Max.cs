2a72b450d8e063a1bc99223b56bb8ba9b67fdba6
MaxXQx <144465756+MaxXQx@users.noreply.github.com>
2023-09-08 17:57:39 +0300

    Add files via upload

---
+++ Max.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lap01_LM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SByte a = -100;
+            SByte a2 = -101;
+            SByte f = Math.Max(a, a2);
+            Console.WriteLine(f);
+        }
+    }
+}
