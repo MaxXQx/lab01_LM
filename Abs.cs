33ad2c488ddb8d77826ba6bd6ff83f0b972cfe88
MaxXQx <144465756+MaxXQx@users.noreply.github.com>
2023-09-08 17:55:56 +0300

    Add files via upload

---
+++ Abs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lap01_LM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SByte a = -100;
+            SByte f = Math.Abs(a);
+            Console.WriteLine(f);
+        }
+    }
+}
