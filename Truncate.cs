fa1b3bbaf3eef37ec195987259c8edea9d904a24
MaxXQx <144465756+MaxXQx@users.noreply.github.com>
2023-09-08 18:05:17 +0300

    Add files via upload

---
+++ Truncate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lap01_LM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = 23.97M;
+            decimal f = Math.Truncate(a);
+            Console.WriteLine(f);
+        }
+    }
+}
