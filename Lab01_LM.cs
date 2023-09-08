1113b84479af9e82318c54ab711963ba68721e39
MaxXQx <144465756+MaxXQx@users.noreply.github.com>
2023-09-08 17:46:56 +0300

    Add files via upload

---
+++ Lab01_LM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lap01_LM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double FG = 4.0;
+            double d = Math.Tanh(FG);
+            Console.WriteLine(d);
+        }
+    }
+}
